7cc8f5488e1ada6824f853770706aabd7ee0245b
Jorge Barrientos <jorge.barrientos@gmail.com>
2019-08-30 17:00:14 -0600

    Add  env vars

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //jshint esversion:6
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -9,14 +11,17 @@ const app = express();
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const secret = "thisIsOurLittleSecret.";
+//console.log(process.env.API_KEY);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"]
+});
 //
 
 const User = new mongoose.model("User", userSchema);
